a1152dbd22ce7012fa05880924e3d065bcec3924
ksu_smolyar <ksu_smolyar@mail.ru>
2025-02-05 18:41:27 +0400

    change imgs

--- src/widgets/chat/ui/ChatPreview.tsx
+++ src/widgets/chat/ui/ChatPreview.tsx
@@ -1,10 +1,11 @@
 import { LockEl } from "../../../shared/ui/LockEl";
 import styles from "../Chat.module.css";
+import previewImg from "../../../../public/whatsappImg.png"
 
 export const ChatPreview = () => {
     return (
         <div className={styles.chatPreview}>
-            <img className={styles.chatPreviewImg} src={"public/whatsappImg.png"} alt={"whatsApp превью"} width={320} />
+            <img className={styles.chatPreviewImg} src={previewImg} alt={"whatsApp превью"} width={320} />
             <h1>WhatsApp для Windows</h1>
             <LockEl />
         </div>
